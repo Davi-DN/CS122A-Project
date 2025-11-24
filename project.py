24b48c5828cb0c713353813b3dba28177dd41c90
Davi-DN <=>
2025-11-23 20:19:41 -0800

    start

---
+++ project.py
@@ -0,0 +1,40 @@
+import sys
+import mysql.connector
+
+def import_(db, folder_name):
+
+    print(f'Foldername - {folder_name}')
+    
+    mycursor = db.cursor()
+    mycursor.execute("SHOW DATABASES")
+
+
+
+    if "AgentPlatform" in mycursor:
+        print("in!")
+
+    print("continuing....")
+
+    
+
+
+def main():
+    # sys.argv
+    # [0] - project.py 
+    # [1] - (im)
+    # [2] - Other Parameters
+    command = sys.argv[1]
+
+    db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=""
+    )
+
+    if command == "import":
+        import_(db, sys.argv[2])
+
+    
+
+if __name__ == "__main__":
+    main()
