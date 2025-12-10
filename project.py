57cfaad2f37b567076d68b4cac81cd9d9a1ba931
David Ngo <ddngo3@uci.edu>
2025-12-09 19:25:49 -0800

    changed addcustmodel

--- project.py
+++ project.py
@@ -144,6 +144,11 @@ def insertAgentClient(uid, username, email, card_number, card_holder, expiration
 def addCustomizedModel(mid, bmid):
     db = connection()
     mycursor = db.cursor()
+    mycursor.execute("SELECT 1 FROM CustomizedModel WHERE uid = %s", (bmid,))
+    result = mycursor.fetchone()
+    if result:
+        print("Fail")
+        return
 
     sql = """
     INSERT INTO CustomizedModel 
