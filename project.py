0d598166899d4b00aec81b96b72db13cbcf624b5
Davi-DN <=>
2025-12-09 22:01:04 -0800

    fix

--- project.py
+++ project.py
@@ -144,7 +144,7 @@ def insertAgentClient(uid, username, email, card_number, card_holder, expiration
 def addCustomizedModel(mid, bmid):
     db = connection()
     mycursor = db.cursor()
-    mycursor.execute("SELECT 1 FROM CustomizedModel WHERE mid = %s", (mid))
+    mycursor.execute("SELECT 1 FROM CustomizedModel WHERE mid = %s", (int(mid),))
     result = mycursor.fetchone()
     if result:
         print("Fail")
